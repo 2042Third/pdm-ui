4c9e2b3cfcd7f2c9a7c8c3b234a52c9c51830492
Mike Yang <18604713262@163.com>
2021-07-13 10:54:27 -0400

    Added new drag and drop feature and a macos binary

---
+++ cmake-build-debug/src/CMakeFiles/pdm.dir/DependInfo.cmake
@@ -0,0 +1,35 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/mikeyang/Downloads/pdm-ui-main/src/auth.cpp" "/Users/mikeyang/Downloads/pdm-ui-main/cmake-build-debug/src/CMakeFiles/pdm.dir/auth.cpp.o"
+  "/Users/mikeyang/Downloads/pdm-ui-main/src/cApp.cpp" "/Users/mikeyang/Downloads/pdm-ui-main/cmake-build-debug/src/CMakeFiles/pdm.dir/cApp.cpp.o"
+  "/Users/mikeyang/Downloads/pdm-ui-main/src/cMain.cpp" "/Users/mikeyang/Downloads/pdm-ui-main/cmake-build-debug/src/CMakeFiles/pdm.dir/cMain.cpp.o"
+  "/Users/mikeyang/Downloads/pdm-ui-main/src/cc20_multi.cpp" "/Users/mikeyang/Downloads/pdm-ui-main/cmake-build-debug/src/CMakeFiles/pdm.dir/cc20_multi.cpp.o"
+  "/Users/mikeyang/Downloads/pdm-ui-main/src/pdmFile.cpp" "/Users/mikeyang/Downloads/pdm-ui-main/cmake-build-debug/src/CMakeFiles/pdm.dir/pdmFile.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "WXUSINGDLL"
+  "_FILE_OFFSET_BITS=64"
+  "__WXMAC__"
+  "__WXOSX_COCOA__"
+  "__WXOSX__"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/Users/mikeyang/Downloads/wxWidgets-3.1.5/buildGTK/lib/wx/include/osx_cocoa-unicode-3.1"
+  "/Users/mikeyang/Downloads/wxWidgets-3.1.5/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/src/CMakeFiles/pdm.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pdm.dir/auth.cpp.o"
+  "CMakeFiles/pdm.dir/cApp.cpp.o"
+  "CMakeFiles/pdm.dir/cMain.cpp.o"
+  "CMakeFiles/pdm.dir/cc20_multi.cpp.o"
+  "CMakeFiles/pdm.dir/pdmFile.cpp.o"
+  "pdm"
+  "pdm.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pdm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
